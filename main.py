04efa61f1e33f54f7a20457f8530c368d7ca2fab
Byteintosh <158021536+Byteintosh@users.noreply.github.com>
2024-03-27 16:40:30 +0700

    Remove password check

--- main.py
+++ main.py
@@ -5,21 +5,10 @@ import tkinter as tk
 
 totp = pyotp.TOTP("secret key")
 
-def check_password():
-    password = password_entry.get()
-    if password == "password here":
-        def update_totp():
-            current_totp = totp.now()
-            totp_label.config(text="Current TOTP: \n" + current_totp)  # Update the label with current TOTP
-            password_label.pack_forget()  # Hide the password label
-            password_entry.pack_forget()  # Hide the password entry
-            submit_button.pack_forget()
-            window.after(1000, update_totp)  # Schedule the next update after 1 seconds
-
-        update_totp()  # Start the initial update
-        access_label.config(text="")  # Clear the access label
-    else:
-        access_label.config(text="Incorrect password. Access denied.")
+def update_totp():
+    current_totp = totp.now()
+    totp_label.config(text="Current TOTP: \n" + current_totp)  # Update the label with current TOTP
+    window.after(1000, update_totp)  # Schedule the next update after 1 second
 
 # Create the main window
 window = tk.Tk()
@@ -31,23 +20,12 @@ screen_width = 256
 screen_height = 128
 window.geometry(f"{screen_width}x{screen_height}")
 
-# Create password label and entry
-password_label = tk.Label(window, text="Enter password:")
-password_label.pack()
-password_entry = tk.Entry(window, show="*")
-password_entry.pack()
-
-# Create access label
-access_label = tk.Label(window, text="")
-access_label.pack()
-
 # Create TOTP label
 totp_label = tk.Label(window, text="", font=("Segoe UI", 20))
 totp_label.pack()
 
-# Create submit button
-submit_button = tk.Button(window, text="Submit", command=check_password)
-submit_button.pack()
+# Start the initial update
+update_totp()
 
 # Start the GUI event loop
 window.mainloop()
