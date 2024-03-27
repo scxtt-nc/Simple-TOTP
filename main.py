e9dec8a75aec9602e1c65d48234699c43defbed5
Byteintosh <158021536+Byteintosh@users.noreply.github.com>
2024-03-27 16:49:35 +0700

    Change to standalone mode

--- main.py
+++ main.py
@@ -3,12 +3,26 @@ import time
 import os
 import tkinter as tk
 
-totp = pyotp.TOTP("secret key")
+def get_secret_key():
+    secret_key = input("Enter the secret key: ")
+    with open("secret.env", "w") as file:
+        file.write(f"SECRET_KEY={secret_key}")
 
 def update_totp():
     current_totp = totp.now()
     totp_label.config(text="Current TOTP: \n" + current_totp)  # Update the label with current TOTP
     window.after(1000, update_totp)  # Schedule the next update after 1 second
+
+# Check if secret.env file exists
+if os.path.isfile("secret.env"):
+    with open("secret.env", "r") as file:
+        secret_key = file.read().split("=")[1]
+else:
+    get_secret_key()
+    with open("secret.env", "r") as file:
+        secret_key = file.read().split("=")[1]
+
+totp = pyotp.TOTP(secret_key)
 
 # Create the main window
 window = tk.Tk()
